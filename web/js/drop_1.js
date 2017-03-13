f39ffa5f41e7ed72ba685cd0853787bb2df604a5
prateek011 <prateek.gupta2014@gmail.com>
2017-03-13 21:04:38 +0530

    drop js is added

---
+++ web/js/drop_1.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+                $("#user1 dt").mouseover(function(){
+                $(".dduser1").css({"display":"block"});
+		    
+                    });
+					 $("#user1 dt").mouseout(function(){
+                $(".dduser1").css({"display":"none"});
+		    
+                    });
+					$(".dduser1").mouseover(function(){
+                $(".dduser1").css({"display":"block"});
+                    });
+					 $(".dduser1").mouseout(function(){
+                $(".dduser1").css({"display":"none"});
+		    
+                    });
+					
+					/*      user2      */
+					
+				   	$("#user2 dt").mouseover(function(){
+                $(".dduser2").css({"display":"block"});
+		    
+                    });
+					 $("#user2 dt").mouseout(function(){
+                $(".dduser2").css({"display":"none"});
+		    
+                    });
+					
+					$(".dduser2").mouseover(function(){
+                $(".dduser2").css({"display":"block"});
+                    });
+					 $(".dduser2").mouseout(function(){
+                $(".dduser2").css({"display":"none"});
+		    
+                    });
+					
+					/*   user3     */
+					$("#user3 dt").mouseover(function(){
+                $(".dduser3").css({"display":"block"});
+		    
+                    });
+					 $("#user3 dt").mouseout(function(){
+                $(".dduser3").css({"display":"none"});
+		    
+                    });
+					
+					$(".dduser3").mouseover(function(){
+                $(".dduser3").css({"display":"block"});
+                    });
+					 $(".dduser3").mouseout(function(){
+                $(".dduser3").css({"display":"none"});
+		    
+                    });
+					
+            });
